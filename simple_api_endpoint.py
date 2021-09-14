25b9264d39a26c2a8b50c9bb653e172f27f4cfd3
Chris Marrison <chris@infoblox.com>
2021-09-14 14:31:43 +0100

    20210914 v0.0.2 Initial Version

---
+++ simple_api_endpoint.py
@@ -0,0 +1,73 @@
+#!/usr/local/bin/python3
+#vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+'''
+
+ Description:
+
+    Simple 'Echo' Enpoint API for Outbound API Testing
+
+ Requirements:
+   Python 3.6+ with Flask
+
+ Author: Chris Marrison
+
+ Date Last Updated: 20210913
+
+ Todo:
+
+ Copyright (c) 2021 Chris Marrison / Infoblox
+
+ Redistribution and use in source and binary forms,
+ with or without modification, are permitted provided
+ that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+
+'''
+__version__ = '0.0.2'
+__author__ = 'Chris Marrison'
+__author_email__ = 'chris@infoblox.com'
+
+import flask
+import json
+
+records = []
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def get():
+	'''
+	Output Records
+	'''
+	return flask.jsonify(records)
+
+
+@app.route('/', methods=['POST'])
+def receive_data():
+	'''
+	Receive data from Outbound API
+	'''
+	data = json.loads(flask.request.data)
+	records.append(data)
+	return data
+
+app.run(host="0.0.0.0")
